592e63f8842fe81fc81b919242cee1221f81cc35
mlwelker <nutria.watts.0g@icloud.com>
2025-01-08 16:31:31 -0600

    a1t3: add "Memorize!" title

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -14,6 +14,9 @@ struct ContentView: View {
     
     var body: some View {
         VStack{
+            Text("Memorize!")
+                .font(.largeTitle)
+                .foregroundStyle(.orange)
             ScrollView {
                 cards
             }
