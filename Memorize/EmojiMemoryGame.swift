6cc398d63a7cb12eefce6b7e87f8498628e12dfe
mlwelker <nutria.watts.0g@icloud.com>
2025-01-09 22:26:24 -0600

    ViewModel: add closure

--- Memorize/EmojiMemoryGame.swift
+++ Memorize/EmojiMemoryGame.swift
@@ -7,7 +7,12 @@ func createCardContent(forPairAtIndex index: Int) -> String {
 }
 
 class EmojiMemoryGame {
-    private var model: MemoryGame<String> = MemoryGame<String>(numberOfPairsOfCards: 4, cardContentFactory: createCardContent)
+    private var model: MemoryGame<String> = MemoryGame<String>(
+        numberOfPairsOfCards: 4,
+        cardContentFactory: { (index: Int) -> String in
+            return ["👻", "🎃", "🕷️", "😈","💀","🕸️","🧙‍♀️","🙀", "👹", "😱", "☠️", "🍭"][index]
+        }
+    )
     
     var cards: Array<MemoryGame<String>.Card> {
         return model.cards
