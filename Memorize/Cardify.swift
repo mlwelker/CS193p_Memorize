a26c5fef86c0fc15897ead58680bf6c2536d94b4
mlwelker <nutria.watts.0g@icloud.com>
2025-01-22 01:24:03 -0600

    add Cardify

---
+++ Memorize/Cardify.swift
@@ -0,0 +1,25 @@
+// Cardify
+
+import SwiftUI
+
+struct Cardify: ViewModifier {
+    let isFaceUp: Bool
+    
+    func body(content: Content) -> some View {
+        ZStack {
+            let base: RoundedRectangle = RoundedRectangle(cornerRadius: Constants.cornerRadius)
+            Group {
+                base.fill(.white)
+                base.strokeBorder(lineWidth: Constants.lineWidth)
+                content
+            }
+            .opacity(isFaceUp ? 1 : 0)
+            base.fill().opacity(isFaceUp ? 0 : 1)
+        }
+    }
+    
+    private struct Constants {
+        static let cornerRadius: CGFloat = 12
+        static let lineWidth: CGFloat = 3
+    }
+}
